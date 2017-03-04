eae3b8314682bf8d40e209f99dee4ecaafcf6c06
Agustin Camino <agustin.camino@gmail.com>
2017-03-03 22:09:20 -0500

    Introduce the `fetchRepositories` function
    This function serves to encapsulate the interaction with the GitHub API.

--- src/Main.hs
+++ src/Main.hs
@@ -1,12 +1,13 @@
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DeriveGeneric     #-}
 
 module Main where
 
+import Control.Arrow ((&&&))
 import Control.Exception (throwIO)
 import Data.Aeson (FromJSON)
 import Data.List (group, sort, sortBy)
-import Data.Maybe (catMaybes)
+import Data.Maybe (mapMaybe)
 import Data.Monoid ((<>))
 import Data.Ord (Down(..), comparing)
 import GHC.Generics
@@ -19,38 +20,39 @@ instance MonadHttp IO where
 
 type Language = T.Text
 data Repository = Repository { language :: Maybe Language
-                             } deriving (Show, Generic)
+                             } deriving (Generic)
 
 instance FromJSON Repository
 
 main = do
-  r <- req GET
-    (https "api.github.com" /: "orgs" /: "stackbuilders" /: "repos")
-    NoReqBody
-    jsonResponse
-    (header "User-Agent" "ghs" <> "per_page" =: T.pack "100")
-  let repos        = responseBody r :: Maybe [Repository]
-      langs        = getLanguages repos
+  repos <- fetchRepositories "stackbuilders"
+  let langs        = mapMaybe language repos
       groupedLangs = group . sort $ langs
       stats        = getStats groupedLangs
   mapM_ T.putStrLn $ histogram stats
 
-getLanguages :: Maybe [Repository] -> [Language]
-getLanguages repos =
-  case repos of
-    Nothing    -> []
-    Just repos -> catMaybes . map language $ repos
+fetchRepositories :: MonadHttp m
+  => T.Text            -- ^ Github organization name
+  -> m [Repository]
+fetchRepositories orgName = do
+  let gitHubApi           = https "api.github.com"
+      listRepositoriesUrl = gitHubApi /: "orgs" /: orgName /: "repos"
+      params =
+        "per_page" =: (100 :: Int) <>
+        header "User-Agent" "ghs"
+  repos <- req GET listRepositoriesUrl NoReqBody jsonResponse params
+  return (responseBody repos :: [Repository])
 
 getStats :: [[Language]] -> [(Language, Int)]
 getStats groupedLangs =
-  let stats = map (\ls -> (head ls, length ls)) groupedLangs
+  let stats = map (head &&& length) groupedLangs
   in sortBy (comparing $ Down . snd) stats
 
 histogram :: [(Language, Int)] -> [T.Text]
 histogram =
-  let bar (language, quantity) = T.pack (replicate quantity '#')
-                              <> " "
-                              <> language
-                              <> " "
-                              <> T.pack (show quantity)
+  let bar (language, quantity) = T.pack (replicate quantity '#') <>
+                                 " "                             <>
+                                 language                        <>
+                                 " "                             <>
+                                 T.pack (show quantity)
   in map bar
