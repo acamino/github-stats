547e877fbaccca4603e4efc4ec52f32a10bd6635
Agustin Camino <agustin.camino@gmail.com>
2017-07-07 01:03:53 -0500

    Get rid of mappend on stats rendering

--- src/Console/GitHubStats/Stats.hs
+++ src/Console/GitHubStats/Stats.hs
@@ -5,26 +5,21 @@ module Console.GitHubStats.Stats where
 import           Control.Arrow
 import           Data.List
 import           Data.Maybe
-import           Data.Monoid
 import           Data.Ord
 import qualified Data.Text                 as T
 
 import           Console.GitHubStats.Types
 
 histogram :: [Repository] -> [T.Text]
-histogram repos = map renderBar stats
+histogram repos = fmap renderBar stats
   where
     renderBar (language, quantity) =
-      T.replicate quantity "#" <>
-      " "                      <>
-      language                 <>
-      " "                      <>
-      T.pack (show quantity)
+      T.unwords [ T.replicate quantity "#"
+                , language
+                , T.pack (show quantity)
+                ]
 
-    stats  = toStats . groupLangs $ repos
+    stats = toStats . groupLangs $ repos
 
-    groupLangs :: [Repository] -> [[Language]]
+    toStats    = sortBy (comparing $ Down . snd) . fmap (head &&& length)
     groupLangs = group . sort . mapMaybe repoLanguage
-
-    toStats :: [[Language]] -> [(Language, Int)]
-    toStats = sortBy (comparing $ Down . snd) . map (head &&& length)
