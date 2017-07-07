d219e8fd8e704608361468c99e2c6d7fe2d27c2f
Agustin Camino <agustin.camino@gmail.com>
2017-07-07 00:38:13 -0500

    Move instances of FromJSON to the Types module
    - Remove redundant type annotations to improve readability

--- src/Console/GitHubStats.hs
+++ src/Console/GitHubStats.hs
@@ -1,14 +1,11 @@
-{-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE RecordWildCards   #-}
-{-# OPTIONS -fno-warn-orphans  #-}
 {-# LANGUAGE DataKinds         #-}
+{-# LANGUAGE OverloadedStrings #-}
 
 module Console.GitHubStats
   ( fetchRepos
   ) where
 
 import           Control.Monad
-import           Data.Aeson
 import           Data.Maybe
 import           Data.Monoid
 import qualified Data.Text                 as T
@@ -16,52 +13,45 @@ import           Network.HTTP.Req
 
 import           Console.GitHubStats.Types
 
-instance FromJSON Repository where
-  parseJSON = withObject "repo in org" $ \o -> do
-    repoLanguage <- o .: "language"
-    return Repository{..}
-
-instance FromJSON Organization where
-  parseJSON = withObject "organization" $ \o -> do
-    orgPublicRepos <- o .: "public_repos"
-    return Organization{..}
-
-perPage :: Integer
-perPage = 100
-
 fetchRepos :: MonadHttp m
   => T.Text            -- ^ Github organization name
   -> m [Repository]
 fetchRepos orgName = do
   numberOfRepos <- fetchNumberOfRepos orgName
-  let n = numberOfRepos `div` perPage
-      pages = case numberOfRepos `mod` perPage of
-              0 -> n
-              _ -> n + 1
+  let n     = numberOfRepos `div` perPage
+      pages = if numberOfRepos `mod` perPage == 0
+                 then n
+                 else n + 1
   repos <- forM [1..pages] $ fetchReposByPage orgName
   return $ concat repos
+
 
 fetchReposByPage :: MonadHttp m
   => T.Text
   -> Integer
   -> m [Repository]
 fetchReposByPage orgName page = do
-  let listReposUrl = gitHubApi /: "orgs" /: orgName /: "repos"
-      params = "per_page" =: perPage <>
-               "page" =: page <>
-               header "User-Agent" "github-stats"
+  let listReposUrl = gitHubApiUrl /: "orgs" /: orgName /: "repos"
+      params       = "per_page" =: perPage <>
+                     "page" =: page <>
+                     header "User-Agent" "github-stats"
   repos <- req GET listReposUrl NoReqBody jsonResponse params
-  return (responseBody repos :: [Repository])
+  return $ responseBody repos
+
 
 fetchNumberOfRepos :: MonadHttp m
   => T.Text
   -> m Integer
 fetchNumberOfRepos orgName = do
-  let listReposUrl = gitHubApi /: "orgs" /: orgName
+  let listReposUrl = gitHubApiUrl /: "orgs" /: orgName
       params       = header "User-Agent" "github-stats"
   repos <- req GET listReposUrl NoReqBody jsonResponse params
-  let organization = responseBody repos :: Organization
-  return $ fromMaybe 0 $ orgPublicRepos organization
+  return $ fromMaybe 0 . orgPublicRepos $ responseBody repos
 
-gitHubApi :: Url 'Https
-gitHubApi = https "api.github.com"
+
+gitHubApiUrl :: Url 'Https
+gitHubApiUrl = https "api.github.com"
+
+
+perPage :: Integer
+perPage = 100
--- src/Console/GitHubStats/Types.hs
+++ src/Console/GitHubStats/Types.hs
@@ -1,6 +1,10 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards   #-}
+
 module Console.GitHubStats.Types where
 
-import qualified Data.Text as T
+import           Data.Aeson
+import qualified Data.Text  as T
 
 type Language = T.Text
 
@@ -8,6 +12,16 @@ data Repository = Repository
   { repoLanguage :: Maybe Language
   }
 
+instance FromJSON Repository where
+  parseJSON = withObject "repo in org" $ \o -> do
+    repoLanguage <- o .: "language"
+    return Repository{..}
+
 data Organization = Organization
   { orgPublicRepos :: Maybe Integer
   }
+
+instance FromJSON Organization where
+  parseJSON = withObject "organization" $ \o -> do
+    orgPublicRepos <- o .: "public_repos"
+    return Organization{..}
